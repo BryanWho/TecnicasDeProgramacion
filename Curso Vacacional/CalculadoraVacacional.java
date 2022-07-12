123e29bf2d70f02f204b4139abe4c555cb928a08
BryanWho <brayanromeroariza@gmail.com>
2022-07-11 21:01:33 -0500

    Calculator

---
+++ Curso Vacacional/CalculadoraVacacional.java
@@ -0,0 +1,14 @@
+/*
+ * Menu Calculadora
+ * 1. Ingresar datos
+ * El programa debe permitirme ingresar datos cuantas 
+ */
+
+import java.util.Scanner;
+public class CalculadoraVacacional {
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        CalculadoraVacacional obj = new CalculadoraVacacional();
+        obj.sc.nextLine();
+    }
+}
