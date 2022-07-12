dbb60b8cc9caabc8c318465d3663aeb3699e1fab
BryanWho <brayanromeroariza@gmail.com>
2022-07-11 21:07:08 -0500

    CalculatorVacacional

--- CalculadoraVacacional.java
+++ CalculadoraVacacional.java
@@ -1,6 +1,10 @@
 /*
  * Menu Calculadora
  * 1. Ingresar datos
+ * 2. Suma
+ * 3. Resta
+ * 4. Multiplicación
+ * 5. División
  * El programa debe permitirme ingresar datos cuantas 
  */
 
@@ -9,6 +13,6 @@ public class CalculadoraVacacional {
     Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         CalculadoraVacacional obj = new CalculadoraVacacional();
-        obj.sc.nextLine();
+        obj.sc.nextLine():
     }
 }
