739c9de72e81341985719754d1879dea7b64af7b
BryanWho <brayanromeroariza@gmail.com>
2022-07-11 21:15:29 -0500

    Segunda Prueba sin carpeta

--- CalculadoraVacacional/src/CalculadoraVacacional.java
+++ CalculadoraVacacional/src/CalculadoraVacacional.java
@@ -1,5 +1,18 @@
-public class App {
+/*
+ * Menu Calculadora
+ * 1. Ingresar datos
+ * 2. Suma
+ * 3. Resta
+ * 4. Multiplicación
+ * 5. División
+ * El programa debe permitirme ingresar datos cuantas 
+ */
+
+import java.util.Scanner;
+public class CalculadoraVacacional {
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CalculadoraVacacional obj = new CalculadoraVacacional();
+        obj.sc.nextLine():
     }
 }
