ef3666870bf8e86373855aef3c37ebdc2072ad1a
BryanWho <brayanromeroariza@gmail.com>
2022-07-14 21:10:53 -0500

    Clase POO

--- GestionBicicletas/src/Bicicleta.java
+++ GestionBicicletas/src/Bicicleta.java
@@ -1,11 +1,13 @@
 public class Bicicleta {
+    private int id;
     private String marca;
     private String tipoModalidad;
     private String color;
     private String tipoGrupo;
     private String marcaRuedas;
 
-    public Bicicleta(String marca, String tipoModalidad, String color, String tipoGrupo, String marcaRuedas){
+    public Bicicleta(int id, String marca, String tipoModalidad, String color, String tipoGrupo, String marcaRuedas){
+        this.id = id;
         this.marca = marca;
         this.tipoModalidad = tipoModalidad;
         this.color = color;
@@ -18,5 +20,53 @@ public class Bicicleta {
         return "Bicicleta [color=" + color + ", marca=" + marca + ", marcaRuedas=" + marcaRuedas + ", tipoGrupo="
                 + tipoGrupo + ", tipoModalidad=" + tipoModalidad + "]";
     }
-    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getTipoModalidad() {
+        return tipoModalidad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getTipoGrupo() {
+        return tipoGrupo;
+    }
+
+    public String getMarcaRuedas() {
+        return marcaRuedas;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setTipoModalidad(String tipoModalidad) {
+        this.tipoModalidad = tipoModalidad;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTipoGrupo(String tipoGrupo) {
+        this.tipoGrupo = tipoGrupo;
+    }
+
+    public void setMarcaRuedas(String marcaRuedas) {
+        this.marcaRuedas = marcaRuedas;
+    }
+
 }
--- GestionBicicletas/src/GestionBicicletas.java
+++ GestionBicicletas/src/GestionBicicletas.java
@@ -1,6 +1,45 @@
+import java.util.Scanner;
+
 public class GestionBicicletas {
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        Bicicleta bicicleta1 = new Bicicleta("Specialized", "Ruta", "Rojo", "Ultegra", "Mavic");
-        System.out.println(bicicleta1);
+        Scanner sc = new Scanner(System.in);
+        //Bicicleta bicicleta1 = new Bicicleta("Specialized", "Ruta", "Rojo", "Ultegra", "Mavic");
+        //System.out.println(bicicleta1);
+        GestionBicicletas obj = new GestionBicicletas();
+        Bicicleta[] listBicicletas = new Bicicleta[2];
+        obj.guardarBicicletas(listBicicletas);
+        System.out.println("Ingrese Id");
+        int idBuscado = Integer.parseInt(obj.sc.nextLine());
+        if(obj.buscarBicicleta(idBuscado, listBicicletas == null)){
+            System.out.println("No se encuentra");
+        }
+        else{
+            System.out.println("Encontrado");
+        }
+    }
+
+    public void guardarBicicletas(Bicicleta[] arreglo){
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese los datos por &: ");
+            System.out.println("Id&marca&modalidad&color&grupo&ruedas");
+            String entrada = sc.nextLine();
+            String[] datos = entrada.split("&");
+            int id = Integer.parseInt(datos[0]);
+            String marca = datos[1];
+            String tipoModalidad = datos[2];
+            String color = datos[3];
+            String tipoGrupo = datos[4];
+            String marcaRuedas = datos[5];
+            Bicicleta bicicletaCopia = new Bicicleta(id, marca, tipoModalidad, color, tipoGrupo, marcaRuedas);
+        }
+    }
+
+    public Bicicleta buscarBicicleta(int id, Bicicleta[] arreglo[]){
+        for (int i = 0; i < arreglo.length; i++) {
+            if(id == arreglo[i].getId()){
+                return arreglo[i];
+            }
+        }
     }
 }
